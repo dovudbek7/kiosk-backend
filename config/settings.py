2e8832df33de2512b86967c048209d2dc08c2141
Dovudbek <dovudbek@Dovudbeks-MacBook-Air.local>
2026-04-17 13:59:56 +0500

    add

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 import os
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # AUTH_USER_MODEL = 'api_v1.User' # Custom user ishlatganimiz uchun
